bf09b5e6d696dd4a49eb862fdd58aec26a1a84c7
Wadah Mac Kraken <wadah-wleed@hotmail.com>
2021-04-21 23:36:34 +0300

    Added electron dev support

--- config/webpack/webpack.config.dev.js
+++ config/webpack/webpack.config.dev.js
@@ -1,17 +1,18 @@
-const path = require('path');
-const WriteFilePlugin = require('write-file-webpack-plugin');
-const baseConfig = require('./webpack.config.base');
-const webpackMerge = require('webpack-merge');
+const path = require("path");
+const WriteFilePlugin = require("write-file-webpack-plugin");
+const baseConfig = require("./webpack.config.base");
+const webpackMerge = require("webpack-merge");
 
 module.exports = webpackMerge.merge(baseConfig, {
-  mode: 'development',
+  mode: "development",
   output: {
     path: path.resolve(__dirname, "../../builds/developmentServer"),
     filename: "bundle.js",
     chunkFilename: "[id].js",
     publicPath: "",
   },
-  plugins: [
-    new WriteFilePlugin(),
-  ],
+  plugins: [new WriteFilePlugin()],
+  devServer: {
+    port: 3000,
+  },
 });
