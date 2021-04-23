52fa7aacd0ce65191e3405868c7bae0d329dfbec
Wadah Mac Kraken <wadah-wleed@hotmail.com>
2021-04-24 02:13:11 +0300

    Added eslint

--- config/updateCapacitorIPAddress.js
+++ config/updateCapacitorIPAddress.js
@@ -1,39 +1,39 @@
-const fs = require("fs");
-const fileName = "../capacitor.config.json";
-const file = require(fileName);
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
+const os = require('os');
+const file = require('../capacitor.config.json');
 
 function getIPAddress() {
-  var interfaces = require("os").networkInterfaces();
-  for (var devName in interfaces) {
-    var iface = interfaces[devName];
+  const interfaces = os.networkInterfaces();
+  for (const devName in interfaces) {
+    const iface = interfaces[devName];
 
-    for (var i = 0; i < iface.length; i++) {
-      var alias = iface[i];
+    for (let i = 0; i < iface.length; i += 1) {
+      const alias = iface[i];
       if (
-        alias.family === "IPv4" &&
-        alias.address !== "127.0.0.1" &&
+        alias.family === 'IPv4' &&
+        alias.address !== '127.0.0.1' &&
         !alias.internal
       )
-        return alias.address;
+        return alias.address; 
     }
   }
-  return "0.0.0.0";
+  return '0.0.0.0';
 }
 
-if (file.server.url.includes("internal-ip-address")) {
+if (file.server.url.includes('internal-ip-address')) {
   file.server.url = `http://${getIPAddress()}:3000`;
 }
 
-console.log({ file });
-
 fs.writeFile(
-  path.join(__dirname, "../capacitor.config.json"),
+  path.join(__dirname, '../capacitor.config.json'),
   JSON.stringify(file, null, 2),
   function writeJSON(err) {
     if (err) {
-      return console.error(err);
+      console.error(err);
+      return false;
     }
     console.log(`Done copying ip address to capacitor config`);
+    return true;
   }
 );
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 
 import Logo from './assets/logo.png';
 
@@ -9,77 +9,79 @@ const App: React.FC = () => {
   const [clickedButton, setClickedButton] = React.useState(``);
   const [counter, setCounter] = React.useState(0);
 
-  document.addEventListener('keydown', function(e) {
-    switch(e.keyCode){
-    case 37:
-      setClickedButton(`LEFT arrow`)
-      break;
-    case 38:
-      setClickedButton(`UP arrow`)
-      break;
-    case 39:
-      setClickedButton(`RIGHT arrow`)
-      break;
-    case 13:
-      setClickedButton(`OK button`)
-      break;
-    case 10009: //RETURN button
-      tizen.application.getCurrentApplication().exit();
-      break;
-    default:
-      setClickedButton(`${e.keyCode}`);
-      break;
+  document.addEventListener('keydown', (e: { keyCode: number }) => {
+    switch (e.keyCode) {
+      case 37:
+        setClickedButton(`LEFT arrow`);
+        break;
+      case 38:
+        setClickedButton(`UP arrow`);
+        break;
+      case 39:
+        setClickedButton(`RIGHT arrow`);
+        break;
+      case 13:
+        setClickedButton(`OK button`);
+        break;
+      case 10009: // RETURN button
+        tizen.application.getCurrentApplication().exit();
+        break;
+      default:
+        setClickedButton(`${e.keyCode}`);
+        break;
     }
   });
 
   return (
     <div
       style={{
-        position: "absolute",
-        width: "100%",
-        height: "100%",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
+        position: 'absolute',
+        width: '100%',
+        height: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
       }}
     >
       <img src={Logo} />
       <div>Welcome to React Multiplatform App (WebOS)</div>
       <p style={{ fontSize: 30, marginTop: 50 }}>{counter}</p>
-      <div style={{ display: "flex", flexDirection: "row" }}>
+      <div style={{ display: 'flex', flexDirection: 'row' }}>
         <button
+          type="button"
           onClick={() => {
             setCounter(counter + 1);
           }}
           style={{
             height: 70,
             width: 70,
-            borderRadius: "50%",
-            backgroundColor: "#61DAFB",
-            borderColor: "gray",
+            borderRadius: '50%',
+            backgroundColor: '#61DAFB',
+            borderColor: 'gray',
             borderWidth: 1,
-            margin: "10px 10px",
+            margin: '10px 10px',
             fontSize: 30,
-            color: "white",
+            color: 'white',
           }}
         >
           +
         </button>
         <button
+          type="button"
           onClick={() => {
             setCounter(counter !== 0 ? counter - 1 : counter);
           }}
           style={{
             height: 70,
             width: 70,
-            borderRadius: "50%",
-            backgroundColor: "#61DAFB",
-            borderColor: "gray",
+            borderRadius: '50%',
+            backgroundColor: '#61DAFB',
+            borderColor: 'gray',
             borderWidth: 1,
-            margin: "10px 10px",
+            margin: '10px 10px',
             fontSize: 30,
-            color: "white",
+            color: 'white',
           }}
         >
           -
@@ -88,6 +90,6 @@ const App: React.FC = () => {
       {clickedButton && <p>{clickedButton}</p>}
     </div>
   );
-}
+};
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+ReactDOM.render(<App />, document.querySelector('#root'));
